92e1fa8d521ec3f3955bd53238200da53b83d27b
Jimmy <jimmy.zhaang@gmail.com>
2025-06-29 18:20:10 -0400

    use supabase schemas

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,5 @@
+import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
