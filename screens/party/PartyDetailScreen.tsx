3b28fa7567e6847bb2404cb29947baea01ca1992
Jimmy <jimmy.zhaang@gmail.com>
2025-07-07 17:26:22 -0400

    add functionality to add members to party

--- screens/party/PartyDetailScreen.tsx
+++ screens/party/PartyDetailScreen.tsx
@@ -1,15 +1,25 @@
-import { getLinksByPartyId, getPartyById } from '@/lib/supabase';
+import {
+  addPartyMember,
+  getLinksByPartyId,
+  getPartyById,
+  getPartyMembers,
+  getUserByEmail,
+  getUserById,
+} from '@/lib/supabase';
 import { RootStackParamList } from '@/navigation/AppNavigator';
 import { Database } from '@/types/supabase';
-import { Button, Container, PressableCard } from '@/ui';
+import { Button, Container, Input, PressableCard } from '@/ui';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useEffect, useState } from 'react';
-import { ActivityIndicator, Text } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator, Alert, Text, View } from 'react-native';
 
 type Route = RouteProp<RootStackParamList, 'PartyDetail'>;
 type Nav = NativeStackNavigationProp<RootStackParamList>;
 type Party = Database['public']['Tables']['parties']['Row'];
+type PartyMember = Database['public']['Tables']['party_members']['Row'] & {
+  users: { name: string | null };
+};
 type Link = Database['public']['Tables']['links']['Row'];
 
 export default function PartyDetailScreen() {
@@ -17,24 +27,56 @@ export default function PartyDetailScreen() {
   const navigation = useNavigation<Nav>();
 
   const [party, setParty] = useState<Party | null>(null);
+  const [members, setMembers] = useState<PartyMember[]>([]);
   const [links, setLinks] = useState<Link[]>([]);
+
+  const [inviteEmail, setInviteEmail] = useState('');
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
+  const fetchData = useCallback(async () => {
+    setLoading(true);
 
-      const party = await getPartyById(partyId);
-      const links = await getLinksByPartyId(partyId);
+    const { data: party } = await getPartyById(partyId);
+    const { data: members } = await getPartyMembers(partyId);
+    const { data: links } = await getLinksByPartyId(partyId);
 
-      setParty(party);
-      setLinks(links);
+    if (party) setParty(party);
+    if (members) setMembers(members);
+    if (links) setLinks(links);
 
-      setLoading(false);
-    };
-
-    fetchData();
+    setLoading(false);
   }, [partyId]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const inviteByEmail = async () => {
+    const { data: inviteUser } = await getUserByEmail(inviteEmail);
+
+    if (!inviteUser) {
+      Alert.alert('User not found');
+      return;
+    }
+
+    const { data: user } = await getUserById(inviteUser.id);
+
+    if (!user) {
+      Alert.alert('User not found');
+      return;
+    }
+
+    const userId = user.id;
+
+    const invitedUser = await addPartyMember({ user_id: userId, party_id: partyId });
+
+    if (!invitedUser) {
+      Alert.alert('Error inviting user.');
+    } else {
+      Alert.alert('User added!');
+      fetchData();
+    }
+  };
 
   if (loading) return <ActivityIndicator />;
 
@@ -42,6 +84,27 @@ export default function PartyDetailScreen() {
     <Container>
       <Text className="text-xl font-bold mb-1">{party?.name ?? 'Unnamed Party'}</Text>
       <Text className="text-sm text-gray-500 mb-4">Party ID: {partyId}</Text>
+
+      <Text className="text-base font-semibold mt-4">Members</Text>
+      <View className="mb-2">
+        {members.map((member) => (
+          <Text key={member.user_id}>• {member.users.name}</Text>
+        ))}
+      </View>
+
+      <Input
+        placeholder="Enter user email"
+        value={inviteEmail}
+        onChangeText={setInviteEmail}
+        keyboardType="email-address"
+        className="mb-4"
+      />
+
+      <Button
+        title={loading ? 'Inviting...' : 'Invite a Friend'}
+        onPress={inviteByEmail}
+        disabled={loading}
+      />
 
       {links.length > 0 ? (
         <>
