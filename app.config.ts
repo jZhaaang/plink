fc3ce492feb2202dd1c5e504e9fadfe25200a4e1
Jimmy <jimmy.zhaang@gmail.com>
2026-02-14 15:49:57 -0500

    eas setup

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,9 @@ export default {
   extra: {
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
+    eas: {
+      projectId: 'adc09ed9-bd33-44d3-bf90-d5ad713f8604',
+    },
   },
   android: {
     package: 'com.anonymous.plink',
